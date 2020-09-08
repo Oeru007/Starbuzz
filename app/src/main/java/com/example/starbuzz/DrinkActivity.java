1b36cc9a68d8ba8b8e95251d715e76c9a156d791
Oeru007 <oeru007@mail.ru>
2020-09-08 04:00:50 +0300

    PPK starbuzz db

--- app/src/main/java/com/example/starbuzz/DrinkActivity.java
+++ app/src/main/java/com/example/starbuzz/DrinkActivity.java
@@ -2,11 +2,14 @@ package com.example.starbuzz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteException;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -23,7 +26,7 @@ public class DrinkActivity extends AppCompatActivity {
         SQLiteOpenHelper starbuzzDatabaseHelper = new StarbuzzDatabaseHelper(this);
         try {
             SQLiteDatabase db = starbuzzDatabaseHelper.getReadableDatabase();
-            Cursor cursor = db.query("DRINKS", new String[] {"NAME", "DESCRIPTION", "IMAGE_RESOURCE_ID"},
+            Cursor cursor = db.query("DRINKS", new String[] {"NAME", "DESCRIPTION", "IMAGE_RESOURCE_ID", "FAVORITE"},
                     "_id=?",
                     new String[] {Integer.toString(drinkId)}, null,
                     null,
@@ -31,17 +34,36 @@ public class DrinkActivity extends AppCompatActivity {
             TextView name = (TextView) findViewById(R.id.name);
             TextView description = (TextView) findViewById(R.id.description);
             ImageView photo = (ImageView) findViewById(R.id.photo);
+            CheckBox favorite = (CheckBox) findViewById(R.id.favorite);
             if(cursor.moveToFirst()){
                 name.setText(cursor.getString(0));
                 description.setText(cursor.getString(1));
                 photo.setImageResource(cursor.getInt(2));
                 photo.setContentDescription(cursor.getString(1));
+                favorite.setChecked(cursor.getInt(3)==1);
             }
             cursor.close();
             db.close();
         } catch (SQLiteException exc) {
             Toast toast = Toast.makeText(this, "Database unavailable",
                     Toast.LENGTH_SHORT);
+            toast.show();
+        }
+    }
+
+    public void onFavoriteClicked(View view) {
+        int drinkId = getIntent().getIntExtra(EXTRA_DRINKID,0);//Extras().get(EXTRA_DRINKID);
+        CheckBox favorite = (CheckBox) findViewById(R.id.favorite);
+        ContentValues drinkValues = new ContentValues();
+        drinkValues.put("FAVORITE", favorite.isChecked());
+
+        SQLiteOpenHelper starbuzzDatabaseHelper = new StarbuzzDatabaseHelper(this);
+        try {
+            SQLiteDatabase db = starbuzzDatabaseHelper.getWritableDatabase();
+            db.update("DRINKS", drinkValues, "_id = ?", new String[] {Integer.toString(drinkId)});
+            db.close();
+        } catch (SQLiteException exc) {
+            Toast toast = Toast.makeText(this, "Database unavailable", Toast.LENGTH_SHORT);
             toast.show();
         }
     }
--- app/src/main/java/com/example/starbuzz/TopLevelActivity.java
+++ app/src/main/java/com/example/starbuzz/TopLevelActivity.java
@@ -3,17 +3,31 @@ package com.example.starbuzz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.CursorAdapter;
 import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.Toast;
 
 public class TopLevelActivity extends AppCompatActivity {
+
+    private Cursor favoritesCursor;
+    private SQLiteDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_top_level);
+        setupOptionsListView();
+        setupFavoritesListView();
+    }
+    private void setupOptionsListView() {
         AdapterView.OnItemClickListener itemClickListener = new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
@@ -25,7 +39,56 @@ public class TopLevelActivity extends AppCompatActivity {
         };
         ListView listView = (ListView) findViewById(R.id.list_options);
         listView.setOnItemClickListener(itemClickListener);
+    }
+    private void setupFavoritesListView(){
+        ListView listFavorite = (ListView) findViewById(R.id.list_favorites);
+        SQLiteOpenHelper starbuzzDatabaseHelper = new StarbuzzDatabaseHelper(this);
+        try {
+            db = starbuzzDatabaseHelper.getReadableDatabase();
+            favoritesCursor = db.query("DRINKS", new String[] {"_id", "NAME"},
+                    "FAVORITE = 1", null, null,
+                    null, null);
+            CursorAdapter cursorAdapter = new SimpleCursorAdapter(TopLevelActivity.this,
+                    android.R.layout.simple_list_item_1,
+                    favoritesCursor,
+                    new String[] {"NAME"}, new int[] {android.R.id.text1}, 0);
+            listFavorite.setAdapter(cursorAdapter);
+        } catch (SQLiteException exc) {
+            Toast toast = Toast.makeText(this, "Database unavailable", Toast.LENGTH_SHORT);
+            toast.show();
+        }
+        listFavorite.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(TopLevelActivity.this, DrinkActivity.class);
+                intent.putExtra(DrinkActivity.EXTRA_DRINKID, (int) id);
+                startActivity(intent);
+            }
+        });
+    }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        favoritesCursor.close();
+        db.close();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        SQLiteOpenHelper openHelper = new StarbuzzDatabaseHelper(this);
+        try {
+            db = openHelper.getReadableDatabase();
+            Cursor cursor = db.query("DRINKS", new String[] {"_id", "NAME"},
+                    "FAVORITE = 1", null, null, null, null);
+            ListView listView = (ListView) findViewById(R.id.list_favorites);
+            CursorAdapter cursorAdapter = (CursorAdapter) listView.getAdapter();
+            cursorAdapter.changeCursor(cursor);
+        } catch (SQLiteException exc) {
+            Toast toast = Toast.makeText(this, "Database unavailable", Toast.LENGTH_SHORT);
+            toast.show();
+        }
 
     }
 }
